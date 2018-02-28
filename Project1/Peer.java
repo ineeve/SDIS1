046bd0467ae0d4f40bfbd0f8792003fcadb70c32
Renato Alexandre Sousa Campos <up201504942@B03AZ0411.fe.up.pt>
2018-02-28 12:37:53 +0000

    Added Peer class

---
+++ Project1/Peer.java
@@ -0,0 +1,11 @@
+
+public class Peer {
+
+	public static void main(String[] args) {
+		//Non initiator peers: java Peer id MC_ip MC_port MDB_ip MDB_port
+		//Initiator peers: java Peer id MC_ip MC_port MDB_ip MDB_port filename replication
+		
+
+	}
+
+}
