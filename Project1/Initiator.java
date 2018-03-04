e24a38c68b780af7bced338c41609704c8ae9987
ineeve <renatoineeve@gmail.com>
2018-03-04 00:26:24 +0000

    removed unused imports

--- Project1/Initiator.java
+++ Project1/Initiator.java
@@ -8,13 +8,8 @@ import java.net.MulticastSocket;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class Initiator implements Runnable {
 
