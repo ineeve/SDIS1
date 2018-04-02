052f1d7644dd22c24d99765de8c5c54a9f314274
Joao611 <joaofvde@gmail.com>
2018-04-03 00:41:28 +0100

    STATE showing used/max disk space

--- Project1/src/PeerState.java
+++ Project1/src/PeerState.java
@@ -7,44 +7,20 @@ import java.util.ArrayList;
 import utils.ThreadUtils;
 
 public class PeerState implements Serializable {
-
-	private ArrayList<BackedUpFile> backedUpFiles;
-//	private ArrayList<StoredChunk> storedChunks;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7200331457829652271L;
 	private long maxDiskSpace; //KBytes
 	private long usedDiskSpace; //KBytes
 	
 	public PeerState(ReplicationStatus repStatus) {
-		backedUpFiles = repStatus.getFiles();
 		maxDiskSpace = repStatus.getBytesReserved() / 1000;
 		usedDiskSpace = repStatus.getBytesUsed() / 1000;
 	}
 
 	public void present() {
-		System.out.println("BACKED UP FILES");
-		for (final BackedUpFile file : backedUpFiles) {
-			file.present();
-		}
-		System.out.println();
-		
-		System.out.println("STORED CHUNKS");
-//		for (final StoredChunk chunk : storedChunks) {
-//			chunk.present();
-//		}
-		System.out.println();
-		
-		System.out.format("Maximum disk space: %d KB\n", maxDiskSpace);
 		System.out.format("Used disk space: %d KB\n", usedDiskSpace);
+		System.out.format("Maximum disk space: %d KB\n", maxDiskSpace);
 	}
-
-//	public void backupFile(String pathname, String fileId, byte desiredRepDegree) {
-//		backedUpFiles.add(new BackedUpFile(pathname, fileId, desiredRepDegree));
-//	}
-//
-//	public void setNumChunks(String fileId, int chunkNo) {
-//		for (BackedUpFile file : backedUpFiles) {
-//			if (file.getFileId() == fileId) {
-//				file.initChunks(chunkNo);
-//			}
-//		}
-//	}
 }
